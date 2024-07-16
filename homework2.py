abc7121d90a16ad0a299560b13c75fa51951a98a
vstoychanov <qwert123456789asdf@mail.ru>
2024-07-16 15:06:04 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+homework_count = 12
+hours_count = 1.5
+course_name = 'Python'
+time_per_task = hours_count/homework_count
+print('Курс:',course_name,', всего задач:',homework_count,', затрачено часов:',hours_count,', среднее время выполнения:',time_per_task, 'часа')
