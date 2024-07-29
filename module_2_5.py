d66d12d965aa7c19b130f632f18365cf44f5a02e
vstoychanov <qwert123456789asdf@mail.ru>
2024-07-29 13:50:03 +0300

    Add files via upload

--- module_2_5.py
+++ module_2_5.py
@@ -3,7 +3,7 @@ def get_matrix(n, m, value):
     for i in range(n):
         matrix.append([])
         for j in range(m):
-            matrix[i].append([value])
+            matrix[i].append(value)
     return matrix
 result1 = get_matrix(2, 2, 10)
 result2 = get_matrix(3, 5, 42)
