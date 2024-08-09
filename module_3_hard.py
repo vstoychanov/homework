6400e87db1568bfede3a9ff88458fa44fb607a9a
Victor <qwert123456789asdf@mail.ru>
2024-08-09 17:29:11 +0300

    homework module 3 hard

--- module_3_hard.py
+++ module_3_hard.py
@@ -1,22 +1,20 @@
-def structure_sum(data_structure):
-    if i(data_structure, int):
-        return data_structure
-
-    elif isinstance(data_structure, (bool, float)):
+def calculate_structure_sum (data_structure):
+    if isinstance(data_structure, (int,float,bool)):
         return data_structure
 
     elif isinstance(data_structure, str):
         return len(data_structure)
 
-    elif isinstance(data_structure, (list, set, tuple)):
-        data_structure = list(data_structure)
-            if len(data_structure) == 0:
-                return 0
-            elif len(data_structure) == 0:
-            return structure_sum(data_structure[0])
-            else:
-            return (structure_sum(data_structure[0]) +
-                    structure_sum(data_structure[1:]))
+    elif isinstance(data_structure, (list,set,tuple)):
+         data_structure = list(data_structure)
+         if len(data_structure)==0:
+             return 0
+
+         elif len(data_structure)==0:
+             return calculate_structure_sum(data_structure[0])
+         else:
+             return (calculate_structure_sum(data_structure[0]) +
+                     calculate_structure_sum(data_structure[1:]))
 
     elif isinstance(data_structure, dict):
          keys = list(data_structure.keys())
@@ -26,12 +24,12 @@ def structure_sum(data_structure):
 
 
 data_structure = [
-    [1, 2, 3],
-    {'a': 4, 'b': 5},
-    (6, {'cube': 7, 'drum': 8}),
-    "Hello",
-    ((), [{(2, 'Urban', ('Urban2', 35))}])
-]
+        [1, 2, 3],
+        {'a': 4, 'b': 5},
+        (6, {'cube': 7, 'drum': 8}),
+        "Hello",
+        ((), [{(2, 'Urban', ('Urban2', 35))}])
+    ]
 
-result = structure_sum(data_structure)
+result = calculate_structure_sum(data_structure)
 print(result)
