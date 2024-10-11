99c6eb17c72f965951faaa326ea3714f8fa37096
Victor <qwert123456789asdf@mail.ru>
2024-10-12 02:50:52 +0300

    hw 10 1

---
+++ module_10_1.py
@@ -0,0 +1,40 @@
+from time import sleep
+from threading import Thread
+from datetime import datetime
+
+
+def write_words(word_count, file_name):
+    with open(file_name, 'w', encoding='utf-8') as file:
+        for i in range(1, word_count + 1):
+            file.write(f"Какое-то слово № {i}\n")
+            sleep(0.1)
+    print(f"Завершилась запись в файл {file_name}")
+
+time_start_func = datetime.now()
+write_words(10, 'example1.txt')
+write_words(30, 'example2.txt')
+write_words(200, 'example3.txt')
+write_words(100, 'example4.txt')
+time_end_func = datetime.now()
+time_res_func = time_end_func - time_start_func
+print(time_res_func)
+
+time_start_thr = datetime.now()
+thr_1 = Thread(target=write_words, args=(10, 'example5.txt'))
+thr_2 = Thread(target=write_words, args=(30, 'example6.txt'))
+thr_3 = Thread(target=write_words, args=(200, 'example7.txt'))
+thr_4 = Thread(target=write_words, args=(100, 'example8.txt'))
+
+thr_1.start()
+thr_2.start()
+thr_3.start()
+thr_4.start()
+
+thr_1.join()
+thr_2.join()
+thr_3.join()
+thr_4.join()
+
+time_end_thr = datetime.now()
+time_res_thr = time_end_thr - time_start_thr
+print(time_res_thr)
