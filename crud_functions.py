0e19855aa0db835c5957a3226d9ea9aeb0bd16c5
vstoychanov <qwert123456789asdf@mail.ru>
2025-01-11 21:08:15 +0300

    hw 14 5

--- crud_functions.py
+++ crud_functions.py
@@ -12,17 +12,44 @@ def initiate_db(db_path='Products.db'):
             price INTEGER NOT NULL
         )
     ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS Users (
+            id INTEGER PRIMARY KEY,
+            username TEXT NOT NULL UNIQUE,
+            email TEXT NOY NULL,
+            age INTEGER NOT NULL,
+            balance INTEGER NOT NULL
+        )
+    ''')
+
+    cursor.execute('DELETE FROM Products')
     all_products = [
         ('Product1', 'Description1', '100'),
         ('Product2', 'Description2', '200'),
         ('Product3', 'Description3', '300'),
         ('Product4', 'Description4', '400')
     ]
-    cursor.executemany('INSERT INTO Products (title, description, price) VALUES (?,?,?)', all_products)
+    for product in all_products:
+        cursor.execute('INSERT OR IGNORE INTO Products (title, description, price) VALUES (?, ?, ?)', product)
     connection.commit()
     connection.close()
 
-initiate_db()
+
+def add_user(username, email, age, db_path='Products.db'):
+    connection = sqlite3.connect(db_path)
+    cursor = connection.cursor()
+    cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, 1000)', (username, email, age))
+    connection.commit()
+    connection.close()
+
+def is_included(username, db_path='Products.db'):
+    connection = sqlite3.connect(db_path)
+    cursor = connection.cursor()
+    cursor.execute('SELECT 1 FROM Users WHERE username = ?', (username,))
+    result = cursor.fetchone()
+    connection.close()
+    return result is not None
 
 def get_all_products(db_path='Products.db'):
     connection = sqlite3.connect(db_path)
@@ -31,3 +58,5 @@ def get_all_products(db_path='Products.db'):
     products = cursor.fetchall()
     connection.close()
     return products
+
+initiate_db()
---
+++ module_14_5.py
@@ -0,0 +1,111 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.dispatcher import FSMContext
+from crud_functions import get_all_products, is_included, add_user
+
+api = '8117805314:AAHoef2Rs_1HbI5dUaNZxqk78fQAd7FcmT0'
+bot = Bot(token = api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+reply_kb = ReplyKeyboardMarkup(resize_keyboard = True)
+reply_button = KeyboardButton(text = 'Рассчитать')
+reply_button2 = KeyboardButton(text = 'Информация')
+reply_button3 = KeyboardButton(text = 'Купить')
+reply_button4 = KeyboardButton(text='Регистрация')
+reply_kb.add(reply_button)
+reply_kb.add(reply_button2)
+reply_kb.add(reply_button3)
+reply_kb.add(reply_button4)
+
+kb = InlineKeyboardMarkup(resize_keyboard = True)
+inline_button = InlineKeyboardButton(text='Product1', callback_data='product_buying')
+inline_button2 = InlineKeyboardButton(text='Product2', callback_data='product_buying')
+inline_button3 = InlineKeyboardButton(text='Product3', callback_data='product_buying')
+inline_button4 = InlineKeyboardButton(text='Product4', callback_data='product_buying')
+kb.add(inline_button)
+kb.add(inline_button2)
+kb.add(inline_button3)
+kb.add(inline_button4)
+
+images = [
+    "1.png",
+    "2.png",
+    "3.png",
+    "4.png"
+]
+
+product = get_all_products()
+
+class RegistrationState(StatesGroup):
+    username = State()
+    email = State()
+    age = State()
+    balance = 1000
+
+@dp.message_handler(commands='start')
+async def starter(message):
+    await message.answer('Привет', reply_markup= reply_kb)
+
+@dp.message_handler(text='Регистрация')
+async def sing_up(message):
+    await RegistrationState.username.set()
+    await message.answer('Введите имя пользователя(только латинский алфавит):')
+
+@dp.message_handler(state=RegistrationState.username)
+async def set_username(message, state):
+    username = message.text
+    if is_included(username):
+        await message.answer('Пользователь существует, введите другое имя:')
+        return
+    await state.update_data(username=username)
+    await RegistrationState.email.set()
+    await message.answer('Введите свой email:')
+
+@dp.message_handler(state=RegistrationState.email)
+async def set_email(message, state):
+    email = message.text
+    await state.update_data(email=email)
+    await RegistrationState.age.set()
+    await message.answer('Введите свой возраст:')
+
+@dp.message_handler(state=RegistrationState.age)
+async def set_age(message, state):
+    age = message.text
+    data = await state.get_data()
+    username = data.get('username')
+    email = data.get('email')
+
+    try:
+        age = int(age)
+        add_user(username, email, age)
+        await message.answer(f'Регистрация успешна! Пользователь {username} добавлен')
+    except ValueError:
+        await message.answer('Пожалуйста введите корректный возраст:')
+        return
+    await state.finish()
+
+@dp.message_handler(text='Купить')
+async def get_buying_list(message):
+    product_list = get_all_products()
+    if product_list:
+        for product, img_path in zip(product_list, images):
+            title, description, price = product[1], product[2], product[3]
+            product_info = f'Навзвание: {title} | Описание: {description} | Цена: {price}'
+            with open(img_path, 'rb') as img:
+                await message.answer_photo(img, caption=product_info)
+    await message.answer('Выберите пролукт для покупки', reply_markup= kb)
+
+@dp.callback_query_handler(text = 'product_buying')
+async def send_confirm_message(call):
+    await call.message.answer('Вы успешно приобрели продукт')
+    await call.answer()
+
+@dp.message_handler()
+async def all_text(message):
+    await message.answer('Введите команду /start')
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
