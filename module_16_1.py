78f866fe70444b41fcc9e5f1a9f07611a5995614
vstoychanov <qwert123456789asdf@mail.ru>
2025-01-18 04:14:03 +0300

    module_16_1.py

--- module_16_1.py
+++ module_16_1.py
@@ -3,17 +3,17 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get('/')
-async def main_page() -> dict:
-    return {"message": "Главная страница"}
+async def main_page() -> str:
+    return f"Главная страница"
 
 @app.get('/user/admin')
-async def admin_page() -> dict:
-    return {'message': 'Вы вошли как администратор'}
+async def admin_page() -> str:
+    return f'Вы вошли как администратор'
 
 @app.get('/user/{user_id}')
-async def user_page(user_id: str) -> dict:
-    return {'message': f'Вы вошли как пользователь № {user_id}'}
+async def user_page(user_id: str) -> str:
+    return f'Вы вошли как пользователь № {user_id}'
 
 @app.get('/user')
-async def user_info(username: str, age: str) -> dict:
-    return {'message': f'Информация о пользователе. Имя: {username}, Возраст: {age}'}
+async def user_info(username: str, age: str) -> str:
+    return f'Информация о пользователе. Имя: {username}, Возраст: {age}'
